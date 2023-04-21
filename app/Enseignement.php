e71a7d13c010b69a2448c1ffdf8eb150c1097fef
amoqOffice <amoq.office@gmail.com>
2023-04-21 20:36:40 +0200

    seeder corrigé

--- app/Enseignement.php
+++ app/Enseignement.php
@@ -8,18 +8,6 @@ class Enseignement extends Model
 {
     protected $guarded = ['id'];
 
-    public function responsables()
-    {
-        return $this->belongsToMany(Responsable::class, 'responsable_enseignement');
-    }
-
-    public function rubriques()
-    {
-        return $this->belongsToMany(Rubrique::class, 'rubrique_enseignement');
-    }
-
+    
     //
-
-
-
 }
